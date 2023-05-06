bcef998fe33be7df267aa99131b2df9cb4eec3f2
Abreham <abrehamamesw@gmail.com>
2023-05-06 22:30:42 +0100

    Complain form changed

--- backend/models/complain.js
+++ backend/models/complain.js
@@ -12,12 +12,7 @@ const ComplainSchema = new Schema(
       ref: "User",
       immutable: true,
     },
-    receiverId: {
-      type: mongoose.Schema.Types.ObjectId,
-      //required: true,
-      ref: "User",
-      immutable: true,
-    },
+   
     title: {
       type: String,
       required: [true, "Title is required"],
@@ -35,10 +30,6 @@ const ComplainSchema = new Schema(
       default: "Pending",
       enum: ["Pending", "Resolved"],
     },
-    assignedTo: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
   },
   { timestamps: true }
 );
@@ -50,12 +41,7 @@ ComplainSchema.pre("save", async function (next) {
       new ErrorResponse(`User not found with id of ${this.user}`, 404)
     );
   }
-  const receiver = await this.model("User").findById(this.receiverId);
-  if (!receiver) {
-    return next(
-      new ErrorResponse(`User not found with id of ${this.receiverId}`, 404)
-    );
-  }
+ 
   next();
 });
 
