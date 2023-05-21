d8a0cc370cc558694938b67bd8f95bd5552c5fec
atinafabebe1 <atinafabebe1@gmail.com>
2023-05-21 11:35:55 +0300

    fetch only vehicle request made by user

--- frontend/src/componenets/pages/employee/VehicleRequests.jsx
+++ frontend/src/componenets/pages/employee/VehicleRequests.jsx
@@ -1,26 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Table, Button, Row, Col, Form } from "react-bootstrap";
-import api from "../../../api/api";
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Table, Button, Row, Col, Form } from 'react-bootstrap';
+import api from '../../../api/api';
+import { useAuth } from '../../../context/AuthContext';
 
 const VehicleRequestListPage = () => {
   const [requests, setRequests] = useState([]);
   const [startIndex, setStartIndex] = useState(0);
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
+  const { user } = useAuth();
+  console.log(user);
 
   useEffect(() => {
     // Fetch the user's vehicle requests from your server API
     api
-      .get("/Request/vehicle")
+      .get(`/Request/vehicle?user=${user.id}`)
       .then((response) => {
         console.log(response.data.data);
+        console.log('user: ' + user);
         setRequests(response.data.data);
       })
-      .catch((error) =>
-        console.error("Error fetching vehicle requests:", error)
-      );
-  }, []);
+      .catch((error) => console.error('Error fetching vehicle requests:', error));
+  }, [user]);
 
   const handleDeleteRequest = (id) => {
     // Delete the vehicle request with the specified ID from your server API
@@ -30,9 +32,7 @@ const VehicleRequestListPage = () => {
         // Filter out the deleted request from the local state
         setRequests(requests.filter((request) => request._id !== id));
       })
-      .catch((error) =>
-        console.error(`Error deleting vehicle requests with ID ${id}:`, error)
-      );
+      .catch((error) => console.error(`Error deleting vehicle requests with ID ${id}:`, error));
   };
 
   const handleNext = () => {
@@ -47,16 +47,13 @@ const VehicleRequestListPage = () => {
   };
 
   const filteredRequests = requests.filter((request) => {
-    return (
-      request.plateNumber.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      request.status.toLowerCase().includes(searchTerm.toLowerCase())
-    );
+    return request.plateNumber.toLowerCase().includes(searchTerm.toLowerCase()) || request.status.toLowerCase().includes(searchTerm.toLowerCase());
   });
   return (
     <div className="p-4">
       <Row className="mb-4">
         <Col>
-          <h4 style={{ color: "#4682B4" }}>Your Last Vehicle Requests</h4>
+          <h4 style={{ color: '#4682B4' }}>Your Last Vehicle Requests</h4>
         </Col>
         <Col className="text-end">
           <Link to="/employee/vehicle-request">
@@ -67,12 +64,7 @@ const VehicleRequestListPage = () => {
       <Form>
         <Row className="mb-3">
           <Col>
-            <Form.Control
-              type="text"
-              placeholder="Search by plate number or status"
-              value={searchTerm}
-              onChange={handleSearch}
-            />
+            <Form.Control type="text" placeholder="Search by plate number or status" value={searchTerm} onChange={handleSearch} />
           </Col>
         </Row>
       </Form>
@@ -100,40 +92,22 @@ const VehicleRequestListPage = () => {
               <td>{request.reason}</td>
               <td>{request.status}</td>
               <td>
-                {request.status === "rejected" && (
-                  <Button
-                    className="btn btn-sm"
-                    variant="secondary"
-                    onClick={() =>
-                      navigate(`/employee/edit-vehicle-request/${request._id}`)
-                    }
-                  >
+                {request.status === 'rejected' && (
+                  <Button className="btn btn-sm" variant="secondary" onClick={() => navigate(`/employee/edit-vehicle-request/${request._id}`)}>
                     Resubmit
                   </Button>
                 )}
-                {request.status === "approved" && (
+                {request.status === 'approved' && (
                   <Button className="btn btn-sm" variant="success" disabled>
                     Approved
                   </Button>
                 )}
-                {request.status === "pending" && (
+                {request.status === 'pending' && (
                   <div>
-                    <Button
-                      className="btn btn-sm"
-                      variant="primary"
-                      onClick={() =>
-                        navigate(
-                          `/employee/edit-vehicle-request/${request._id}`
-                        )
-                      }
-                    >
+                    <Button className="btn btn-sm" variant="primary" onClick={() => navigate(`/employee/edit-vehicle-request/${request._id}`)}>
                       Edit
-                    </Button>{" "}
-                    <Button
-                      className="btn btn-sm"
-                      variant="danger"
-                      onClick={() => handleDeleteRequest(request._id)}
-                    >
+                    </Button>{' '}
+                    <Button className="btn btn-sm" variant="danger" onClick={() => handleDeleteRequest(request._id)}>
                       Delete
                     </Button>
                   </div>
@@ -144,22 +118,10 @@ const VehicleRequestListPage = () => {
         </tbody>
       </Table>
       <div className="d-flex justify-content-center align-items-center w-100">
-        <Button
-          variant="primary"
-          className="btn-sm mx-2"
-          onClick={handlePrevious}
-          disabled={startIndex === 0}
-          block
-        >
+        <Button variant="primary" className="btn-sm mx-2" onClick={handlePrevious} disabled={startIndex === 0} block>
           Previous
         </Button>
-        <Button
-          variant="primary"
-          className="btn-sm mx-2"
-          onClick={handleNext}
-          disabled={startIndex + 7 >= requests.length}
-          block
-        >
+        <Button variant="primary" className="btn-sm mx-2" onClick={handleNext} disabled={startIndex + 7 >= requests.length} block>
           Next
         </Button>
       </div>
