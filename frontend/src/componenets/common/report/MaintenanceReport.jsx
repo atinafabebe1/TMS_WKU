8e672bf354ea9f11abc0cb3e8bbca4829ecafe46
Abreham-Amesa <abrehamamesw@gmail.com>
2023-05-31 23:02:38 +0300

    HOD Maintenance report

---
+++ frontend/src/componenets/common/report/MaintenanceReport.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import GDMaintenanceReportPage from "../maintenance/MaintenanceReports";
+const HDMaintenanceReport = () => {
+  return (
+   <div>
+    <GDMaintenanceReportPage/>
+   </div>
+  );
+};
+
+export default HDMaintenanceReport;
