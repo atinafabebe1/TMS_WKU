643a30532f14d349924898a2fb2b0ee7ad1d30a1
keyeron16 <89837030+keyeron16@users.noreply.github.com>
2023-06-02 04:42:51 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -100,7 +100,7 @@ app.use(errorHandler);
 
 // connect to MongoDB
 mongoose
-  .connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
